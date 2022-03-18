00a78d7a8591ffe56b20b0349fb360b019b362d5
StonedAndroid <sashnet778@gmail.com>
2022-03-18 20:09:35 +0300

    init commit

---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+    private Vector3 startPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + Time.deltaTime * speed);
+        if (Vector3.Distance(startPos, transform.position) >= distance) Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnerScript : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    [SerializeField] private float time;
+    [SerializeField] private float speed;
+    [SerializeField] private float distance;
+
+    public InputField[] inputFields;
+
+    private void Start()
+    {
+        inputFields[0].text = time.ToString();
+        inputFields[1].text = speed.ToString();
+        inputFields[2].text = distance.ToString();
+        StartCoroutine(SpawnCoroutine());
+    }
+
+    private IEnumerator SpawnCoroutine()
+    {
+        while (true)
+        {
+            GameObject cube = Instantiate(cubePrefab, transform.position, Quaternion.identity);
+            var cubeScript = cube.GetComponent<CubeScript>();
+            cubeScript.speed = speed;
+            cubeScript.distance = distance;
+            float timer = 0;
+            while (timer < time)
+            {
+                timer += Time.deltaTime;
+                yield return null;
+            }
+        }
+    }
+
+    public void SetTime()
+    {
+        time = float.Parse(inputFields[0].text);
+    }
+
+    public void SetSpeed()
+    {
+        speed = float.Parse(inputFields[1].text);
+    }
+
+    public void SetDistance()
+    {
+        distance = float.Parse(inputFields[2].text);
+    }
+}
